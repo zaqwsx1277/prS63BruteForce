ecbbbe3f77976036b42508387992c5afce97beed
Alexey <github@ppz7.com>
2019-09-23 16:05:42 +0300

    Добавлена форма сервера

---
+++ server/Папка/TLogModel.cpp
@@ -0,0 +1,6 @@
+#include "TLogModel.hpp"
+
+TLogModel::TLogModel()
+{
+
+}
---
+++ server/Папка/TLogModel.hpp
@@ -0,0 +1,19 @@
+#ifndef TLOGMODEL_HPP
+#define TLOGMODEL_HPP
+
+#include <QAbstractTableModel>
+#include <QString>
+
+#include <array>
+
+#include "TCommonDefine.hpp"
+
+class TLogModel : public QAbstractTableModel
+{
+public:
+    TLogModel();
+
+//    std::array <commonDefine::typeDefItem, 2> f ;
+};
+
+#endif // TLOGMODEL_HPP
